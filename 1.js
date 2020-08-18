fda892e85597d42a0e861a542ae18185113403b2
unknown <jerson_carranza2@hotmail.com>
2020-08-17 23:06:08 -0500

    listo

---
+++ 1.js
@@ -0,0 +1,12 @@
+
+
+function palindroma(){
+    console.log("listo");
+   // alert("listo");
+   // var a = document.getElementById("text").value;
+   // console.log("listo"+a);
+}
+
+function myFunction(element,clr) {
+    element.style.color = clr;
+}
